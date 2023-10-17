d656d86a74e6c2fca911a779edb4e3b92b2f76dd
Victor Nunes <victorngomes@gmail.com>
2023-10-16 22:29:57 -0300

    new features added such as photo upload, navbar and feed page

--- myproject/routes.py
+++ myproject/routes.py
@@ -36,11 +36,20 @@ def createaccount():
 @app.route("/profile/<user_id>", methods=["GET", "POST"])  # pass a variable called 'usuario', to load a specific user page
 @login_required
 def profile(user_id):
-    if int(user_id) == int(current_user.id):
+    if int(user_id) == int(current_user.id):  # if the current user is the logged user
         form_photo = FormPhoto()
         if form_photo.validate_on_submit():
             file = form_photo.photo.data
-            safe_name = secure_filename(file.filename)
+            safe_name = secure_filename(file.filename)  # making the filename secure to storage without crashing
+            # salvar o arquivo na pasta
+            file_path = os.path.join(os.path.abspath(os.path.dirname(__file__)),  # getting current file path folder
+                                app.config["UPLOAD_FOLDER"],  # concatenating with static/posts_photos
+                                safe_name)  # setting file name as secure file
+            file.save(file_path)
+            # registering photo in database
+            photo = Photo(image=safe_name, user_id=current_user.id)
+            database.session.add(photo)
+            database.session.commit()
         return render_template("profile.html", user=current_user, form=form_photo)  # user is the same variable to use inside html file
     else:
         user = User.query.get(int(user_id))
@@ -52,3 +61,10 @@ def profile(user_id):
 def logout():
     logout_user()
     return redirect(url_for("homepage"))
+
+
+@app.route("/feed")
+@login_required
+def feed():
+    photos = Photo.query.order_by(Photo.creation_date.desc()).all()
+    return render_template("feed.html", photos=photos)
